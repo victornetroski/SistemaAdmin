f22ea80d500e5aaa7ae730daf2e69401aa5505cc
Victor Netro <victornetros@gmail.com>
2025-03-20 17:06:56 -0600

    Se crea aplicacion para subir xml3

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tasks import views
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
