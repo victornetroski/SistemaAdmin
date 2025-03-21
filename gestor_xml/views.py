cbf089e25bdb5efd11ef87da7eee8f1965946767
Victor Netro <victornetros@gmail.com>
2025-03-21 15:39:50 -0600

    mover aplicacion xml33331122333333321

--- gestor_xml/views.py
+++ gestor_xml/views.py
@@ -24,6 +24,114 @@ logger = logging.getLogger(__name__)
 def home(request):
 
     return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET' :
+        return render(request, 'signup.html', {
+                'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm, 
+                    'error' : 'El usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+              'form': UserCreationForm,
+              'error': 'Las contraseñas no coinciden. Intenta nuevamente'
+        
+        })
+
+@login_required
+def tasks(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request,'tasks.html', {'tasks':tasks})
+
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request,'tasks.html', {'tasks':tasks})
+
+@login_required
+def create_task(request):
+
+    if request.method == 'GET':
+        return render(request, 'create_task.html',{
+            'form': TaskForm
+         })     
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request,'create_task.html',{
+                'form': TaskForm,
+                'error': 'Proporciona datos validos'
+         })     
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404 (Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request,'task_detail.html', {'task':task, 'form':form})
+    else:
+        try:
+            
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request,'task_detail.html', {'task':task, 'form':form,
+            'error': "Error al actualizar" })
+
+@login_required       
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect ('tasks')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect ('tasks')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrecta'
+            })
+        else:
+            login(request, user)
+            return redirect("tasks")
         
 def procesar_xml(file):
 
