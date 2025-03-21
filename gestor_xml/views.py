f10ff1398e0cfecff1c551dbb4076733da37eef7
Victor Netro <victornetros@gmail.com>
2025-03-21 17:01:17 -0600

    mover aplicacion xml33333

--- gestor_xml/views.py
+++ gestor_xml/views.py
@@ -20,63 +20,12 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-def home(request):
-
-    return render(request, 'home.html')
-
-
-def signup(request):
-
-    if request.method == 'GET' :
-        return render(request, 'signup.html', {
-                'form': UserCreationForm
-        })
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('tasks')
-            except IntegrityError:
-                return render(request, 'signup.html', {
-                    'form': UserCreationForm, 
-                    'error' : 'El usuario ya existe'
-                })
-        return render(request, 'signup.html', {
-              'form': UserCreationForm,
-              'error': 'Las contraseñas no coinciden. Intenta nuevamente'
-        
-        })
 
 @login_required
 def gestor_xml(request):
     return render(request, 'gestor_xml/gestor_xml.html')
 
 
-
-def signin(request):
-    if request.method == 'GET':
-        return render(request, 'signin.html', {
-            'form': AuthenticationForm
-        })
-    else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'signin.html', {
-                'form': AuthenticationForm,
-                'error': 'Usuario o contraseña incorrecta'
-            })
-        else:
-            login(request, user)
-            return redirect("home")
-        
-@login_required
-def signout(request):
-    logout(request)
-    return redirect('home')
-        
 def procesar_xml(file):
 
     try:
