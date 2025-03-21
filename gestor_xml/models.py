f7e686dc581592578c5009c381c017349f12e723
Victor Netro <victornetros@gmail.com>
2025-03-21 15:17:51 -0600

    mover aplicacion xm1121

--- gestor_xml/models.py
+++ gestor_xml/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created =models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + '- by' + self.user.username
+    
+class XMLFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
