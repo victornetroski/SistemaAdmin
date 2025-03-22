25b92470c9efd22a0f62694a70a6e29044f8084b
Victor Netro <victornetros@gmail.com>
2025-03-21 21:44:10 -0600

    intento1

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from tasks import views as task_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', task_views.home, name='home'),  # Ruta para la página principal
--- gestor_xml/urls.py
+++ gestor_xml/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.upload_xml, name='upload_xml'),  # Página principal de tareas
     path('upload_xml/', views.upload_xml, name='upload_xml'),
+
     
 ]
