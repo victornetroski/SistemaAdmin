b5e534c31c400b3f8a2f82f46e1f6f2422351883
Victor Netro <victornetros@gmail.com>
2025-03-21 21:48:39 -0600

    intento2

--- gestor_xml/urls.py
+++ gestor_xml/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_xml, name='upload_xml'),  # Página principal de tareas
+    path('gestor_xml/', views.upload_xml, name='upload_xml'),  # Página principal del gestor xml
     path('upload_xml/', views.upload_xml, name='upload_xml'),
 
     
