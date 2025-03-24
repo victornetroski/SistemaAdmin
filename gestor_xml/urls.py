e81712f6e93b851c261ede2dfe962fd2533879b2
Victor Netro <victornetros@gmail.com>
2025-03-24 11:31:04 -0600

    mostrar datos xml21

--- gestor_xml/urls.py
+++ gestor_xml/urls.py
@@ -4,4 +4,5 @@ from . import views  # Importa las vistas de tu aplicación
 urlpatterns = [
     path('upload_xml/', views.upload_xml, name='upload_xml'),  # Ruta para subir el XML
     path('generate_pdf/', views.generate_pdf, name='generate_pdf'),  # Ruta para generar el PDF
+    path('gestor_xml/', views.upload_xml, name='gestor_xml'),  # Ruta para mostrar la página con los datos extraídos
 ]
