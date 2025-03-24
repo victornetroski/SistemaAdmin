b5f6aa7bb656916a89a4a7180ea697daaf850f7d
Victor Netro <victornetros@gmail.com>
2025-03-24 09:04:50 -0600

    guardar datos xml3

--- gestor_xml/admin.py
+++ gestor_xml/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Comprobante, Emisor, Receptor, Concepto, Traslado, Impuestos, Complemento
 
 # Registrar los modelos en el admin
-admin.site.register(Comprobante)
+
 admin.site.register(Emisor)
 admin.site.register(Receptor)
 admin.site.register(Concepto)
