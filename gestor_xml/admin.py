75e63aeee015bf629e902e311f1e0ae983ab5dad
Victor Netro <victornetros@gmail.com>
2025-03-24 09:17:02 -0600

    guardar datos xml33

--- gestor_xml/admin.py
+++ gestor_xml/admin.py
@@ -3,16 +3,10 @@ from .models import Comprobante, Emisor, Receptor, Concepto, Traslado, Impuestos
 
 # Registrar los modelos en el admin
 
+admin.site.register(Comprobante)
 admin.site.register(Emisor)
 admin.site.register(Receptor)
 admin.site.register(Concepto)
 admin.site.register(Traslado)
 admin.site.register(Impuestos)
 admin.site.register(Complemento)
-
-@admin.register(Comprobante)
-class ComprobanteAdmin(admin.ModelAdmin):
-    list_display = ('fecha', 'total', 'tipo_comprobante')  # Campos que se mostrarán en la tabla
-    search_fields = ('folio')  # Campos que se pueden buscar
-    list_filter = ('forma_pago', 'tipo_comprobante')  # Filtros disponibles
-
